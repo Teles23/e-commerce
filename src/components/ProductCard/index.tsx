1c93e39c8c62212a260908f6076214f9f8b336e1
Thiago <92751944+Teles23@users.noreply.github.com>
2024-01-11 12:02:55 -0300

    adicionando botoes de inclusão e remoção do item no carrinho e a funcionalidade de exibir o total

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import { Product } from "../../types/produto";
 import "./style.css";
 
 interface ProductCardProps {
   product: Product;
+  onAddToCart: (productId: number) => void;
+  onRemoveFromCart: (productId: number) => void;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  const { title, price, description, image } = product;
+const ProductCard: React.FC<ProductCardProps> = ({
+  product,
+  onAddToCart,
+  onRemoveFromCart,
+}) => {
+  const { id, title, price, description, image } = product;
+  const [quantity, setQuantity] = useState(0);
+
+  const handleAddToCart = () => {
+    onAddToCart(id);
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+  const handleRemoveFromCart = () => {
+    if (quantity > 0) {
+      onRemoveFromCart(id);
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
 
   return (
     <div className="product-card">
@@ -15,6 +34,11 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
         <h2 className="product-title">{title}</h2>
         <p className="product-description">{description}</p>
         <p className="product-price">R$ {price}</p>
+        <div className="cart-actions">
+          <button onClick={handleRemoveFromCart}>-</button>
+          <span>{quantity}</span>
+          <button onClick={handleAddToCart}>+</button>
+        </div>
       </div>
     </div>
   );
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -4,8 +4,15 @@ import "./style.css";
 import ProductCard from "../../components/ProductCard";
 import { Product } from "../../types/produto";
 
+interface CartItem {
+  productId: number;
+  quantity: number;
+}
+
 function Main() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,12 +27,57 @@ function Main() {
     fetchData();
   }, []);
 
+  const addToCart = (productId: number) => {
+    const existingItem = cart.find((item) => item.productId === productId);
+
+    if (existingItem) {
+      const updatedCart = cart.map((item) =>
+        item.productId === productId
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, { productId, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (productId: number) => {
+    const updatedCart = cart.map((item) =>
+      item.productId === productId
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
+    setCart(updatedCart.filter((item) => item.quantity > 0));
+  };
+
+  useEffect(() => {
+    const totalPrice = cart.reduce((acc, item) => {
+      const product = products.find((p) => p.id === item.productId);
+      if (product) {
+        return acc + product.price * item.quantity;
+      }
+      return acc;
+    }, 0);
+    setTotalPrice(totalPrice);
+  }, [cart, products]);
+
   return (
     <div className="product">
       <div className="product-list">
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard
+            key={product.id}
+            product={product}
+            onAddToCart={addToCart}
+            onRemoveFromCart={removeFromCart}
+          />
         ))}
+      </div>
+      <div className="cart-summary">
+        <h2>Resumo do Carrinho</h2>
+        <p>Total: ${totalPrice.toFixed(2)}</p>
+        <button>Checkout</button>
       </div>
     </div>
   );
